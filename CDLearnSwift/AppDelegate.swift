68b29f5d99e31628d387be86a31baaaffb61567f
bbigcd <bbigcd@163.com>
2017-03-27 00:47:58 +0800

    Create Project
    Created Project of learn swift project, the all demo are from 故胤道长’s
    swift30project

--- CDLearnSwift/AppDelegate.swift
+++ CDLearnSwift/AppDelegate.swift
@@ -12,10 +12,13 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow.init(frame:UIScreen.main.bounds)
+        self.window?.rootViewController = BaseViewContrller()
+        self.window?.makeKeyAndVisible()
         return true
     }
 
---
+++ CDLearnSwift/BaseViewContrller.swift
@@ -0,0 +1,47 @@
+//
+//  BaseViewContrller.swift
+//  CDLearnSwift
+//
+//  Created by chendi on 17/3/27.
+//  Copyright © 2017年 chendi. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewContrller: UIViewController {
+    var tableView = UITableView()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.blue
+        tableView.frame = self.view.bounds
+        tableView.reloadData()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+}
+
+extension BaseViewContrller : UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier: String = "cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        return cell
+    }
+}
+
+extension BaseViewContrller : UITableViewDelegate{
+    
+}
