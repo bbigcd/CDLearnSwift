9c0a6482f5afbc6c795faed7c2ba1ab0cd600f15
bbigcd <bbigcd@163.com>
2017-03-31 23:57:50 +0800

    add imageView and label to scrollView
    add imageView and label to scrollView

--- CDLearnSwift/UIKit&Foundation/UIKitViewController.swift
+++ CDLearnSwift/UIKit&Foundation/UIKitViewController.swift
@@ -10,22 +10,35 @@ import UIKit
 
 class UIKitViewController: UIViewController{
     var scrollView = UIScrollView()
+    var headImageView = UIImageView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.cyan
         initScrollView()
+        addUIToScrollView()
+        view.backgroundColor = UIColor.white
         // Do any additional setup after loading the view.
     }
 
     func initScrollView(){
-        scrollView = UIScrollView.init(frame: self.view.bounds)
+        scrollView = UIScrollView.init(frame: view.bounds)
         scrollView.delegate = self as? UIScrollViewDelegate
-        scrollView.contentSize = self.view.bounds.size
-        scrollView.backgroundColor = UIColor.red
+        scrollView.contentSize = view.bounds.size
         view.addSubview(scrollView)
     }
-
+    
+    func addUIToScrollView(){
+        let frame = CGRect.init(x: 0, y: 0, width: view.bounds.size.width, height: 125)
+        headImageView = UIImageView.init(frame: frame)
+        headImageView.image = UIImage.init(named: "header-contact")
+        scrollView.addSubview(headImageView)
+        
+        let label1Frame = CGRect.init(x: 0, y: 135, width: view.bounds.size.width, height: 40)
+        let label1 = UILabel.init(frame : label1Frame)
+        label1.textAlignment = NSTextAlignment.center
+        label1.text = "测试"
+        scrollView.addSubview(label1)
+    }
 
 }
 
