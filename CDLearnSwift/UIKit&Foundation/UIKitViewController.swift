46916f4d9d618e158fa4f0976eaf3ee3eb27f6d3
bbigcd <bbigcd@163.com>
2017-04-03 23:55:18 +0800

    add label
    add label

--- CDLearnSwift/UIKit&Foundation/UIKitViewController.swift
+++ CDLearnSwift/UIKit&Foundation/UIKitViewController.swift
@@ -33,11 +33,20 @@ class UIKitViewController: UIViewController{
         headImageView.image = UIImage.init(named: "header-contact")
         scrollView.addSubview(headImageView)
         
-        let label1Frame = CGRect.init(x: 0, y: 135, width: view.bounds.size.width, height: 40)
+        let label1Frame = CGRect.init(x: 0, y: 145, width: view.bounds.size.width, height: 40)
         let label1 = UILabel.init(frame : label1Frame)
         label1.textAlignment = NSTextAlignment.center
-        label1.text = "测试"
+        label1.text = "About Us"
+        label1.font = UIFont.init(name: "Damascus", size: 28)
         scrollView.addSubview(label1)
+        
+        let label2Frame = CGRect.init(x: 50, y: 165, width: view.bounds.size.width - 100, height: 125)
+        let label2 = UILabel.init(frame: label2Frame)
+        label2.textAlignment = NSTextAlignment.left
+        label2.text = "Good as Old Phones returns the phones of  yesteryear back to their original glory and then gets them into the hands* of those who appreciate them most:"
+        label2.numberOfLines = 0
+        scrollView.addSubview(label2)
+        
     }
 
 }
