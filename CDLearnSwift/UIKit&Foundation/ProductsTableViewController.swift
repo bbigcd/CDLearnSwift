fcf16018e111d99b3872a56d5ee9c79ae2d5b765
bbigcd <bbigcd@163.com>
2017-04-05 23:53:04 +0800

    add didSelectRowAt func
    add didSelectRowAt func

--- CDLearnSwift/UIKit&Foundation/ProductsTableViewController.swift
+++ CDLearnSwift/UIKit&Foundation/ProductsTableViewController.swift
@@ -68,7 +68,10 @@ class ProductsTableViewController: UITableViewController {
         return cell;
     }
     
-
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        NSLog("\(indexPath.row)")
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
